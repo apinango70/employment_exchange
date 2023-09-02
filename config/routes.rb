2b86b3eb75de5f4f914745d7ab93ba6d337c8e51
Alejandro Piñango <alejandropinango@gmail.com>
2023-09-01 22:30:42 -0400

    se generó el  devise:controllers users para poder personalizar el controlador y agregar a strong params el campo :name

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   get 'pages/index'
-  devise_for :users
+  #devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#index"
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions', # Personaliza el controlador de sesiones
+    registrations: 'users/registrations', # Personaliza el controlador de registros
+    # Agrega otros controladores personalizados aquí si los has generado
+  }
+  
 end
